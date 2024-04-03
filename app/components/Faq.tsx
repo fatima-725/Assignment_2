ad7f2caac55ca7b21ae0d3854ab79e2c6090e897
fatima <fatima.tz8490@gmail.com>
2024-04-03 14:30:46 +0500

    assignment 2

---
+++ app/components/Faq.tsx
@@ -0,0 +1,88 @@
+"use client";
+import * as Accordion from "@radix-ui/react-accordion";
+import Image from "next/image";
+import Plus from "../../public/assets/Plus.svg";
+// import Minus from "../../public/assets/"
+
+const items = [
+  {
+    question: "Ut enim ad minim veniam, quis nostrud exercitation ullam",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+  {
+    question: "Ut enim ad minim veniam, quis nostrud exercitation ullam",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+  {
+    question: "Ut enim ad minim veniam, quis nostrud exercitation ullam",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+  {
+    question: "Ut enim ad minim veniam, quis nostrud exercitation ullam",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+  {
+    question: "Ut enim ad minim veniam, quis nostrud exercitation ullam",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+];
+
+export function Faq() {
+  return (
+    <div className="flex flex-col w-full py-[48px] lg:py-[60px] lg:flex-row lg:gap-x-6">
+      <div className="lg:w-1/3 lg:py-[32px] lg:pr-[56px]">
+        <h3 className="text-[#EB2891] text-[14px] font-medium lg:text-base">
+          Frequently Asked Questions
+        </h3>
+        <h1 className="py-4 text-2xl font-medium text-[172026] lg:text-[42px] lg:leading-[58px]">
+          Lets clarify some of your questions
+        </h1>
+        <p className="text-[#36485C] pb-[24px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore
+        </p>
+      </div>
+
+      <div className="lg:w-2/3">
+        <Accordion.Root
+          type="single"
+          defaultValue="item-1"
+          collapsible
+          className="flex flex-col gap-y-4"
+        >
+          {items.map((item, index) => (
+            <div key={index}>
+              <Accordion.Item
+                value={`item-${index + 1}`}
+                className="bg-[#E3F1FF] p-[16px] rounded-[8px]"
+              >
+                <Accordion.Header>
+                  <Accordion.Trigger className="flex items-center w-full justify-between">
+                    <p className="text-left font-medium text-[#172026] lg:text-[18px]">
+                      {item.question}
+                    </p>
+                    <span>
+                      <Image
+                        src={Plus}
+                        alt="+"
+                        className="h-10 w-10 lg:w-6 lg:h-6"
+                      />
+                    </span>
+                  </Accordion.Trigger>
+                </Accordion.Header>
+                <Accordion.Content>
+                  <p className="pt-2 text-[#36485C]">{item.answer}</p>
+                </Accordion.Content>
+              </Accordion.Item>
+            </div>
+          ))}
+        </Accordion.Root>
+      </div>
+    </div>
+  );
+}
